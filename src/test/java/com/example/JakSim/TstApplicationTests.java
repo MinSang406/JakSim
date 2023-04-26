adbdff28a69b87ca3d7bbb52503e317811b79806
JorimJoram <wkdgyfla97@gmail.com>
2023-04-26 14:04:26 +0900

    Springboot 2.7.11

---
+++ src/test/java/com/example/JakSim/TstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JakSim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
