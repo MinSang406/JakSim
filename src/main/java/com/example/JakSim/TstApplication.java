e54e4fea4ef53c84ca4a73f2a131bbdcfc432403
JorimJoram <wkdgyfla97@gmail.com>
2023-04-26 14:53:49 +0900

    연결 테스트4

--- src/main/java/com/example/JakSim/TstApplication.java
+++ src/main/java/com/example/JakSim/TstApplication.java
@@ -9,6 +9,7 @@ public class TstApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(TstApplication.class, args);
+		//Jorim
 		//I AM JOJANG ^^
 		//반짝2조 화이팅입니다....^^
 		// 왜이럼
