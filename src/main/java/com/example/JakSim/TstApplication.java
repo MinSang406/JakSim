de5324cc61458ace0c604d8a3061b12c493759b8
JorimJoram <wkdgyfla97@gmail.com>
2023-04-29 11:44:29 +0900

    Commit Test

--- src/main/java/com/example/JakSim/TstApplication.java
+++ src/main/java/com/example/JakSim/TstApplication.java
@@ -5,17 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TstApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TstApplication.class, args);
-		//Jorim
-		//I AM JOJANG ^^
-		//반짝2조 화이팅입니다....^^
-		//Jorim
-		// 왜이럼
-		//test
 	}
-	//syum 되라 제발
-	// 이번에는 되려나 ,,?
 }
 
