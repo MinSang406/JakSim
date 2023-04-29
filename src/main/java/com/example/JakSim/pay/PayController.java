63f683d23f9aadc043d6b5699130ae9227f10d50
JorimJoram <wkdgyfla97@gmail.com>
2023-04-29 11:44:29 +0900

    Commit Test

---
+++ src/main/java/com/example/JakSim/pay/PayController.java
@@ -0,0 +1,12 @@
+package com.example.JakSim.pay;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PayController {
+    @GetMapping("/pay")
+    public String BeforePay(){
+        return "content/pay/Pay";
+    }
+}
--- src/test/java/com/example/JakSim/TstApplicationTests.java
+++ src/test/java/com/example/JakSim/TstApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.JakSim;
 
+import com.example.JakSim.login.Member;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class TstApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	@DisplayName("Getter/Setter테스트")
+	void GetterSetterTest(){
+		Member member = new Member();
+		member.setId(1L);
+		member.setEmail("wkdgyfla97@naver.com");
+		member.setName("장효림");
+		member.setPassword("알려주겠냐고 아 ㅋㅋ");
+		member.setRegisterDateTime(LocalDateTime.now());
+
+		System.out.println(member.toString());
+		System.out.println(member.getEmail() + " :: " + member.getRegisterDateTime());
+		// 커밋 테스트
 	}
 
 }
